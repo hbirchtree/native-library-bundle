2d4a4d2358d0e516808ff9d1791252b7ff0e4e25
Håvard Bjerke <h0wy36@gmail.com>
2019-03-24 00:04:54 +0100

    - Exclude non-existent libraries from deployment

--- deploy-script.sh
+++ deploy-script.sh
@@ -5,7 +5,11 @@ INSTALL_BASE_DIR=$BUILD_DIR/$BUILDVARIANT/install
 for a in ${ASSETS[@]}; do
     ASSET="${a}_${BUILDVARIANT}.tar.gz"
 
-    tar -zcvf "$ASSET" -c $INSTALL_BASE_DIR/$a \
+    if [[ ! -d "$INSTALL_BASE_DIR/$a" ]]; then
+        continue
+    fi
+
+    tar -zcvf "$ASSET" -C $INSTALL_BASE_DIR/$a \
         $INSTALL_BASE_DIR/$a
 
     deploy_asset "${ASSET}" ${TARGET_SLUG}
