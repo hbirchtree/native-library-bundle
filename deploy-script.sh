8cd2869a55bc5f8e1a11b5465a6e45f78e17eba3
Håvard Bjerke <h0wy36@gmail.com>
2019-03-23 23:48:48 +0100

    - Fix deploy script filenames

--- deploy-script.sh
+++ deploy-script.sh
@@ -3,7 +3,7 @@ ASSETS=(openal bullet assimp squish glslang shaderc spirvcross)
 INSTALL_BASE_DIR=$BUILD_DIR/$BUILDVARIANT/install
 
 for a in ${ASSETS[@]}; do
-    ASSET="$a_$BUILDVARIANT.tar.gz"
+    ASSET="${a}_${BUILDVARIANT}.tar.gz"
 
     tar -zcvf "$ASSET" -c $INSTALL_BASE_DIR/$a \
         $INSTALL_BASE_DIR/$a
